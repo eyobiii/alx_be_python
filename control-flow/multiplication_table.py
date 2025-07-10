3382cc3663387d9fbd0e5e67a73c133adfef4328
eyobiii <eyobbelayneh28@gmail.com>
2025-07-10 15:59:45 -0700

    last commet to correct the error

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table:").strip())
 
 for x in range(1,11):
-    print(f"{number} x {x} = {number * x}")
+    print(f"{number} * {x} = {number * x}")
